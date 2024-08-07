8529b4e5584e4da38d5514867e69cd84bf4aa39d
Nguyen Le Cong Quy <nguyenlecongquy@gmail.com>
2024-08-07 11:20:00 +0700

    add cors into server

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express, {Request, Response, NextFunction} from "express";
 import * as dotenv from 'dotenv'
 import routes from "./routes";
 dotenv.config()
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 routes(app);
 
